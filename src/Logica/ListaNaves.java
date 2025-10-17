c800243bb1242b0bdd39e937689eded2f80e3f68
Federico Piretro <federicopiretro@outlook.com>
2025-10-17 15:05:01 -0300

    nuevas funciones

--- src/Logica/ListaNaves.java
+++ src/Logica/ListaNaves.java
@@ -11,6 +11,10 @@ public class ListaNaves {
         this.listaNaves = new ArrayList<>();
     }
 
+    public List<Nave> getListaNaves() {
+        return listaNaves;
+    }
+    
     public void agregarNave(Nave nave){
         listaNaves.add(nave);
     }
@@ -22,12 +26,34 @@ public class ListaNaves {
         }
     }
     
-    public void eliminarNavePorNombre(String nombre){
-        
-        
-        
+    public boolean eliminarNavePorNombre(String nombre){
+        for (int i = 0; i < getListaNaves().size(); i++){
+            Nave naveActual = getListaNaves().get(i);
+            if(naveActual.getNombre().equals(nombre)){
+                listaNaves.remove(i);
+                return true;
+            }}
+        return false;
     }
         
+    public boolean comprobacionRepetidoNombre(String nombre){
+        for (int i = 0; i < getListaNaves().size(); i++){
+            Nave naveActual = getListaNaves().get(i);
+            if(naveActual.getNombre().equalsIgnoreCase(nombre))
+                return true;
+        } 
+        return false;
+    }
+    
+    public TiposDeNave tipoDeNave(String nombre){
+        for (int i = 0; i < getListaNaves().size(); i++){
+            Nave naveActual = getListaNaves().get(i);
+            if(naveActual.getNombre().equalsIgnoreCase(nombre)){
+                return naveActual.getTipoNave();
+            }
+        }   
+        return null;
+    }
     
     
 }
