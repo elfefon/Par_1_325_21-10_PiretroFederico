7a6d26496ca37f5dfc9fcff68d1b51497701d560
Federico Piretro <federicopiretro@outlook.com>
2025-10-19 17:02:52 -0300

    modularizacion de inputs

--- src/Logica/Sistema.java
+++ src/Logica/Sistema.java
@@ -1,20 +1,71 @@
 package Logica;
 
+import java.util.Scanner;
+
 public class Sistema{
   
     //manejo y simplificacion de input
                         
     public int inputTipoNave(String tipo){
         
-        if (TiposDeNave.naveExploracion.name().equalsIgnoreCase(tipo)){
+        String input = tipo.toLowerCase();
+        
+        if (input.contains("exploracion")){
             return 1;
-        }else if (TiposDeNave.cargueros.name().equalsIgnoreCase(tipo)){
+        }else if (input.contains("carguero")){
             return 2;
-        }else if (TiposDeNave.crucerosEstelares.name().equalsIgnoreCase(tipo)){
+        }else if (input.contains("crucero estelar")){
             return 3;
         }else {
             return 0;
         }
     }
-        
+    
+    public String ingresoNombre(Scanner sc){
+        boolean corriendo = true;
+        do{
+            try{
+                System.out.println("Ingrese nombre: ");
+                String nombre = sc.nextLine();
+                corriendo = false;
+                return nombre;
+            }catch(Exception e){
+                System.out.println("Ingrese un nombre");
+            }
+        }while(corriendo == true);
+        return null;
+    }
+    
+    public int ingresoAño(Scanner sc){
+        boolean corriendo = true;
+        do{
+            try{
+                System.out.println("Ingrese año de lanzamiento: ");
+                int año = Integer.parseInt(sc.nextLine());
+                corriendo = false;
+                return año;
+            }catch(NumberFormatException e){
+                System.out.println("Ingrese un año numerico.");
+            }
+        }while(corriendo == true);
+        return 0;
+    }
+    
+    public int ingresoCapacidadTripulacion(Scanner sc){
+        boolean corriendo = true;
+        do{
+            try{
+                System.out.println("Ingrese capacidad de tripulacion: ");
+                int capacidadT = Integer.parseInt(sc.nextLine());
+                corriendo = false;
+                return capacidadT;
+            }catch(NumberFormatException e){
+                System.out.println("Ingrese un numero.");
+            }
+        }while(corriendo == true);
+        return 0;
+    }
+    
+    
+    
 }
